f308317a895b7857f7dc5989b25bdf865cdd9376
Dani Coleman <danicoleman00@gmail.com>
2021-02-25 18:03:45 -0700

    Create init within the api directory and define our location-microservice app
    Co-Authored-By: Callbritton <Callbritton@users.noreply.github.com>
    Co-Authored-By: Joshua Carey <jdcarey128@users.noreply.github.com>

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,22 @@
 from flask_restful import Api
-from flask import Flask
+from flask import Flask, jsonify
+# from api.resources.format import ZipCodes
+# from api.services.zip_codes_service import ZipCodesService
 
-def create_app():
+def location_microservice():
+    app = Flask(__name__)
+
+    # create new tone from dream journal entry text
+    @app.route('our/route/here', methods=['POST'])
+    def get_zip_codes():
+
+        zip_code = str(request.data)
+
+        some_variable = ZipCodesService.get_zip_codes(zip_code)
+
+        # Could be refactored
+        zip_code_results = jsonify({'some_variable': some_variable}).json
+
+        return Format.format_zip_code_results(zip_code_results)
+
+    return app
