e927597f9fdd15c2eca173e2e3bc8b59de8c8c01
Joshua Carey <jdcarey128@gmail.com>
2021-02-26 19:58:46 -0700

    Add load_dotenv() for environment variable

--- api/services/zip_code_service.py
+++ api/services/zip_code_service.py
@@ -1,11 +1,12 @@
 import os
 import requests
+from dotenv import load_dotenv
 # from <externalApi> import <endpoint we want>
 
 class ZipCodeService:
     @classmethod
     def get_zip_codes(self, zipcode, radius):
+        load_dotenv()
         key = os.environ.get('ZIPCODE_API_KEY')
-        # We need to load the API_KEY here
         response = requests.get(f'https://www.zipcodeapi.com/rest/{key}/radius.json/{zipcode}/{radius}/miles?minimal')
-        return {"response": response.text}
+        return response.json()
