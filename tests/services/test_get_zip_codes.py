a067901e50ca453d0554dff28a047e1fef5f6a31
Joshua Carey <jdcarey128@gmail.com>
2021-02-26 19:59:18 -0700

    Refactor service test to pass

--- tests/services/test_get_zip_codes.py
+++ tests/services/test_get_zip_codes.py
@@ -7,8 +7,7 @@ class ZipCodeServiceTest(unittest.TestCase):
     expected_zip_codes = ["80015","80013","80014","80040","80041","80042","80044","80046","80047","80017","80247","80012","80011","80045"]
     radius = 5
     response = ZipCodeService.get_zip_codes(zip_code, radius)
-    result = response
-    # result = response['response']
+    result = response['zip_codes']
     self.assertEqual(expected_zip_codes, result)
 
 if __name__ == '__main__':
